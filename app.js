a2ad5417b433107eff5b3a9ed97b0f6fcdda7fbd
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-05-04 12:20:46 +0900

    マウスオーバー・マウスアウトjsのみ

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000ff',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp()
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000ff'})
+  })
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#ff0000'})
   })
 })
